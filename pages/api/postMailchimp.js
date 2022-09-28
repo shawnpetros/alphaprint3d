2da7921c8b90102854886898945f2298a818529c
Shawn Petros <spetros@dealerinspire.com>
2022-09-27 23:13:44 -0700

    logs are good

--- pages/api/postMailchimp.js
+++ pages/api/postMailchimp.js
@@ -5,15 +5,20 @@ mailchimp.setConfig({
 });
 
 export default function handler(req, res) {
+  console.log("add user running", req.body);
+  console.log("apiKey", process.env.API_KEY);
+  console.log("server", process.env.SERVER);
   try {
     const response = mailchimp.lists.addListMember("0933557fe1", {
       email_address: req.body.email,
       status: "subscribed",
     });
 
+    console.log("success!");
+
     res.status(201).json({ response });
   } catch (error) {
-    console.log("ERROR------------------------------==================", {
+    console.log("whoops", {
       error,
     });
 
